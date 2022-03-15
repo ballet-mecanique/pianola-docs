aca4a44cbd53fd0ab51e47ccdfbb34292e3bfbd9
Andrew Blackwell <andrew@balletmecanique.eu>
2022-03-15 08:05:38 +0100

    initial commit

---
+++ navigation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'About Pianola' => [
+        'url' => 'docs/about-pianola',
+        'children' => [
+            'What is Pianola?' => 'docs/what-is-pianola',
+            'Navigation' => 'docs/navigation',
+            'Algolia DocSearch' => 'docs/algolia-docsearch',
+            'Custom 404 Page' => 'docs/custom-404-page',
+        ],
+    ],
+    'Getting Started' => [
+        'url' => 'docs/getting-started',
+        'children' => [
+            'Customizing Your Site' => 'docs/customizing-your-site',
+            'Navigation' => 'docs/navigation',
+            'Algolia DocSearch' => 'docs/algolia-docsearch',
+            'Custom 404 Page' => 'docs/custom-404-page',
+        ],
+    ],
+    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+];
